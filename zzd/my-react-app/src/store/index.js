f425cb7aa8dadb883a976f4a322205481fa55dec
zhoumowubuzuoye <www.1078637049@qq.com>
2022-08-26 17:28:25 +0800

    提交

---
+++ zzd/my-react-app/src/store/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: xiewenhao
+ * @Date: 2022-08-25 15:12:44
+ * @LastEditTime: 2022-08-26 17:25:57
+ * @Description: 
+ */
+import {
+    combineReducers
+} from 'redux'
+
+export default combineReducers({})
